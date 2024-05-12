cc0f74e41fcdb7cd2d86a8321fb26b5043a02852
kiko2183 <romankulicnij@gmail.com>
2024-05-12 03:10:58 +0300

    1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,39 @@
-const formData{
-    email: "",
-    message: ""
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+
+  const formData = {
+    email: '',
+    message: '',
+  };
+
+  function updateFormData() {
+    formData.email = emailInput.value;
+    formData.message = messageInput.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }
+
+  const savedFormData = localStorage.getItem('feedback-form-state');
+  if (savedFormData) {
+    const parsedFormData = JSON.parse(savedFormData);
+    emailInput.value = parsedFormData.email;
+    messageInput.value = parsedFormData.message;
+  }
+
+  form.addEventListener('input', updateFormData);
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (formData.email === '' || formData.message === '') {
+      alert('Please fill out all fields.');
+    } else {
+      console.log(formData);
+      localStorage.removeItem('feedback-form-state');
+      emailInput.value = '';
+      messageInput.value = '';
+    }
+  });
+});
